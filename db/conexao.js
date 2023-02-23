3dbc11b44cd6ea256626611a676530f21097604f
Narcisoosilva <narcisoosilva@gmail.com>
2023-02-22 21:20:29 -0300

    Colocando  variável de ambiente do banco de dados

--- db/conexao.js
+++ db/conexao.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-const uri = process.env.DATABASE_URL || 'mongodb://localhost:27017/acoes-million'
+const uri = process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost:27017/acoes-million';
 
 mongoose.connect(uri, {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
